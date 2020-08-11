005c08c249be2a12407c24bd6c53beb2d9c81773
t394r <tegarp00@gmail.com>
2020-08-11 22:54:37 +0700

    berhasil menampilkan semua data mahasiswa

--- koneksi.js
+++ koneksi.js
@@ -9,8 +9,17 @@ const conn = mysql.createConnection({
 });
 
 conn.connect((err) => {
-    if(err) throw err;
-    console.log('Mysql terkoneksi')
+    // if(err) throw err; 
+        console.log('Mysql terkoneksi');
 });
+
+// Membuka koneksi ke database MySQL
+// conn.connect(function(err){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log('Koneksi dengan id '+ connection.threadId);
+//     }
+// });
 
 module.exports = conn;
